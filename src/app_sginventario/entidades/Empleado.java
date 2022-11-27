c588dd58b55996adc0c687edbb3d2b356897a513
Johana Carrizo <johanacarrizo20@gmail.com>
2022-11-27 14:11:26 -0300

    Probando subir desde otra rama

---
+++ src/app_sginventario/entidades/Empleado.java
@@ -0,0 +1,13 @@
+package app_sginventario.entidades;
+
+public class Empleado {
+    
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String domicilio;
+    private String telefono;
+    
+    
+    
+}
