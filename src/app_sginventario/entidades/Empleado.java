a52a719f47a95541bb4e7116894bb5610db36632
Johana Carrizo <johanacarrizo20@gmail.com>
2022-12-01 16:27:45 -0300

    Subiendo desde rama johaanC

--- src/app_sginventario/entidades/Empleado.java
+++ src/app_sginventario/entidades/Empleado.java
@@ -1,12 +1,111 @@
 package app_sginventario.entidades;
 
-public class Empleado {
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Empleado implements Serializable {
     
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id; 
     private String nombre;
     private String apellido;
     private String domicilio;
     private String telefono;
+    
+    @Enumerated(EnumType.STRING)
+    private Rol rol;
+    
+    @OneToOne
+    private Usuario usuario;
+    
+    @OneToOne
+    private Empresa empresa;
+
+    public Empleado() {
+    }
+
+    public Empleado(int id, String nombre, String apellido, String domicilio, String telefono, Rol rol, Usuario usuario, Empresa empresa) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.domicilio = domicilio;
+        this.telefono = telefono;
+        this.rol = rol;
+        this.usuario = usuario;
+        this.empresa = empresa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
     
     
     
